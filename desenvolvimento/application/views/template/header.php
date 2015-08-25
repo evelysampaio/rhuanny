fdb2ace3cfbd7ee662db5a7db9a25b74dd581479
rhuanvcsantiago mac <rhuanvcsantiago@gmail.com>
2015-08-25 18:44:35 -0300

    estruturação de pastas
    - adicionado htdocs para redirecionar da raiz para a pasta
    desenvolvimento
    
    - adicionado link para logout no header.php

--- desenvolvimento/application/views/template/header.php
+++ desenvolvimento/application/views/template/header.php
@@ -168,7 +168,7 @@
                       <a href="#" class="btn btn-default btn-flat">Profile</a>
                     </div>
                     <div class="pull-right">
-                      <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                      <a href="<?php echo base_url("login/logout");?>" class="btn btn-default btn-flat">Logout</a>
                     </div>
                   </li>
                 </ul>
